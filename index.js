83ea2a751068ccd1012658d48cd7d6d93f6030a6
chinmay5631006 <chinmay@nimapinfotech.com>
2023-03-28 11:04:20 +0530

    Completed

---
+++ index.js
@@ -0,0 +1,33 @@
+const { totalCount } = require('./worker1');
+const express = require('express');
+const { Worker } = require('worker_threads');
+
+const app = express();
+
+app.get('/find', (req, res) => {
+  const worker = new Worker('./worker1.js');
+
+  worker.on('message', (msg) => {
+    if (msg.progress) {
+      console.log(`Exported ${msg.progress} of ${totalCount}`);
+    }
+
+    if (msg.done) {
+      console.log('Data exported to Excel successfully!');
+      res.send('Data exported to Excel successfully!');
+      worker.terminate();
+    }
+  });
+
+  worker.on('error', (err) => {
+    console.error(err);
+    res.status(500).send('An error occurred while exporting data to Excel!');
+    worker.terminate();
+  });
+
+  worker.postMessage({ cmd: 'start', chunks: [500] });
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
---
+++ worker1.js
@@ -0,0 +1,71 @@
+const { DataTypes } = require('sequelize');
+const ExcelJS = require('exceljs');
+const Sequelize = require('sequelize');
+const { parentPort } = require('worker_threads');
+
+const connection = new Sequelize('conversion', 'root', 'abcd@123', {
+  dialect: 'mysql',
+  logging: false
+});
+
+const User = connection.define('user', {
+  name: {
+    type: DataTypes.STRING,
+  },
+  phone: {
+    type: DataTypes.STRING
+  }
+}, {
+  freezeTableName: true
+});
+
+async function exportDataToExcel(chunks) {
+  const workbook = new ExcelJS.Workbook();
+  const sheet = workbook.addWorksheet(`My_Sheet`);
+
+  const chunkSize = chunks[0];
+  const totalCount = await User.count();
+  let offset = 0;
+  let remainingCount = totalCount;
+
+  while (remainingCount > 0) {
+    const data = await User.findAll({
+      offset,
+      limit: chunkSize
+    });
+
+    const jsonData = data.map(user => user.dataValues);
+
+    if (offset === 0) {
+      const headerRow = Object.keys(jsonData[0]);
+      sheet.addRow(headerRow);
+    }
+
+    jsonData.forEach((item) => {
+      sheet.addRow(Object.values(item));
+    });
+
+    await workbook.xlsx.writeFile(`./excel/data.xlsx`);
+    remainingCount -= jsonData.length;
+    offset += chunkSize;
+
+    if(chunkSize > remainingCount){
+      offset = chunkSize + remainingCount;
+    }
+    // console.log(remainingCount, offset, totalCount);
+    let progress = `chunk size 500`;
+    
+    parentPort.postMessage({ progress });
+  }
+
+  parentPort.postMessage({ done: true });
+}
+
+connection.sync({ alter: true })
+  .then(() => {
+    parentPort.on('message', (msg) => {
+      if (msg.cmd && msg.cmd === 'start') {
+        exportDataToExcel(msg.chunks);
+      }
+    });
+  });
