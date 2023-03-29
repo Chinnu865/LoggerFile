057b25526af402a862b974c3f79b7ec2b80013ed
chinmay5631006 <chinmay@nimapinfotech.com>
2023-03-29 18:55:01 +0530

    added functionality

--- worker2.js
+++ worker2.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const { Worker } = require('worker_threads');
 const Sequelize = require('sequelize');
 const { DataTypes } = require('sequelize');
 const fs = require('fs');
@@ -23,33 +21,7 @@ const User = connection.define('user', {
     freezeTableName:true
 });
 
-const app = express();
-
-app.get('/find', (req, res) => {
-  const worker = new Worker('./worker1.js');
-
-  worker.on('message', (msg) => {
-    if (msg.progress) {
-      console.log(`Exported ${msg.progress}% of data`);
-    }
-
-    if (msg.done) {
-      console.log('Data exported to Excel successfully!');
-      res.send('Data exported to Excel successfully!');
-      worker.terminate();
-    }
-  });
-
-  worker.on('error', (err) => {
-    console.error(err);
-    res.status(500).send('An error occurred while exporting data to Excel!');
-    worker.terminate();
-  });
-
-  worker.postMessage({ cmd: 'start', chunks: [500] });
-});
-
-
+const fs = require("fs");
 const ws = fs.createWriteStream("output.csv");
 
 app.get("/exportcsv", async (req, res) => {
@@ -62,7 +34,7 @@ app.get("/exportcsv", async (req, res) => {
       });
   
       const jsonData = data.map(user => user.dataValues);
-      // console.log("jsonData", jsonData);
+      console.log("jsonData", jsonData);
 
       //csv
       fastcsv
@@ -71,10 +43,4 @@ app.get("/exportcsv", async (req, res) => {
           console.log("Write to output.csv successfully!");
         })
         .pipe(ws);
-
-    res.send("exported")
-});
-
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
 });
